12a4b3f1d5ced177c0b017ba6aabe3833625a261
Roberto aguero <rjaguero00@gmail.com>
2018-09-05 13:52:25 -0700

    add local storage to ES5 verison

--- assets/js/book.js
+++ assets/js/book.js
@@ -82,6 +82,9 @@ document.getElementById('book-form').addEventListener('submit',
             //Add book to list
             ui.addBookToList(book);
 
+            //Add to Local Storage
+            addBook(book);
+
             //Success
             ui.showAlert('Book Added!', 'success');
 
@@ -93,6 +96,9 @@ document.getElementById('book-form').addEventListener('submit',
         e.preventDefault();
     });
 
+//DOM load event
+document.addEventListener('DOMContentLoaded', displayBooks());
+
 //Event Listener for delete book
 document.getElementById('book-list').addEventListener('click', function (e) {
     //Instantiate UI
@@ -100,13 +106,58 @@ document.getElementById('book-list').addEventListener('click', function (e) {
     //Delete book
     ui.deleteBook(e.target);
 
+    //Remove from Local storage
+    removeBooks(e.target.parentElement.previousElementSibling.textContent);
+
     //Show message
     ui.showAlert('Book Removed', 'success');
 
     e.preventDefault();
 });
 
+//Get books to local storage
+function getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+        books = [];
+    } else {
+        books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+}
 
+//Add book to local Storage
+function addBook(book) {
+    const books = getBooks();
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+}
+
+//Display books from local storage
+function displayBooks() {
+    const books = getBooks();
+
+    books.forEach(function (book) {
+        const ui = new UI;
+
+        //Add book to UI
+        ui.addBookToList(book);
+    })
+}
+
+//Remove book from local storage
+function removeBooks(isbn) {
+    const books = getBooks();
+
+
+    books.forEach(function (book, index) {
+        if (book.isbn === isbn) {
+            books.splice(index, 1);
+        }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+}
 
 //Responsive navbar
 function openDrawerMenu() {
